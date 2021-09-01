3cfcf334ad77a26de4e2760c32c274376677b1f0
Julien Moreau <julien@thylo.be>
2021-09-01 16:05:21 +0200

    traditional webapp

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const nunjucks = require("nunjucks");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+app.set('view engine', 'njk');
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -17,6 +24,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
-var listener = app.listen(8080, function() {
+const listener = app.listen(8080, function() {
   console.log("Listening on port " + listener.address().port);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const nunjucks = require("nunjucks");
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+const data = [
+  { content: "Plut le chien est beau", created_at: Date.now() },
+  {
+    content: "JMO is just great",
+    created_at: Date.now(),
+  },
+  {
+    content: "Le web c'est pas du stoemp",
+    created_at: Date.now(),
+  },
+  {
+    content: "Le web dynamique c'est chouette",
+    created_at: Date.now(),
+  },
+];
 
 router.get("/notes", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  const notes = req.query.term
+    ? data.filter((note) => note.content.includes(req.query.term))
+    : data;
+  res.render("notes", {
+    notes,
+    title: 'Application web "traditionelle"',
+    term: req.query.term,
+  });
+});
+
+router.post("/notes", function (req, res, next) {
+  data.push({ content: req.body.content, created_at: Date.now() });
+  res.redirect("/notes");
 });
 
 module.exports = router;
